a3b02019e63f97dbb133e2f586b4a2a356e56f40
Juan Esteban Robles <juanestebanrobles3103@gmail.com>
2020-05-28 12:04:26 -0500

    Viewmodel y controller para Respuestas
    Se plantea el prototipo inicial para administrar las respuestas junto a su respectivo ViewModel

---
+++ ProyectoSaberProWeb/Controllers/RespuestasController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoSaberProWeb.Models;
+using ProyectoSaberProWeb.Models.ViewModels;
+
+namespace ProyectoSaberProWeb.Controllers
+{
+    public class RespuestasController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+        // POST: Respuestas/Create
+
+        /// <summary>
+        /// Registra la serie de respuestas de un estudiante
+        /// </summary>
+        /// <param name="pregunta_Estudiante"></param>
+        /// <returns></returns>
+        
+        public ActionResult Create()
+        {
+            RespondeCompetenciaPruebaViewModel rcpv = new RespondeCompetenciaPruebaViewModel(db);
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Pregunta_Estudiante pregunta_Estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.preguntas_estudiantes.Add(pregunta_Estudiante);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pregunta_Estudiante);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
