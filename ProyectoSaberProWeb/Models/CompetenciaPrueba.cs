c5298ccc28686a8824ab3f9b939475f878c2c09c
MarlyAcosta <marlyacosta.a3@gmail.com>
2020-06-02 21:19:39 -0500

    Modelo CompetenciaPrueba incluido
    Se incluyo el modelo para poder determinar cuando una competencia ya fue respondida

---
+++ ProyectoSaberProWeb/Models/CompetenciaPrueba.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoSaberProWeb.Models
+{
+    [Table("CompetenciaPrueba")]
+    public class CompetenciaPrueba
+    {
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "Estado")]
+        public EstadoCompetencia Estado { get; set; }
+        [Required]
+        [ForeignKey("Competencia")]
+        [Display(Name = "Competencia completada")]
+        public int CompetenciaId { get; set; }
+        public virtual Competencia Competencia { get; set; }
+        [Required]
+        [ForeignKey("User")]
+        [Display(Name = "Estudiante que presento")]
+        public int UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        
+        [Required]
+        [ForeignKey("Prueba")]
+        [Display(Name = "Prueba a la que pertenece")]
+        public int PruebaId { get; set; }
+        public virtual Prueba Prueba { get; set; }
+    }
+
+}
---
+++ ProyectoSaberProWeb/Models/EstadoCompetencia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoSaberProWeb.Models
+{
+    public enum EstadoCompetencia
+    {
+        [Description("Presentada")]
+        Presentada = 1,
+        [Description("Sin Responder")]
+        SinResponder = 2
+    }
+}
