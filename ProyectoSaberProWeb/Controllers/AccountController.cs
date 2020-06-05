e1593ae270da2dadc153189ddff12b8fac8356fd
Juan Esteban Robles <juanestebanrobles3103@gmail.com>
2020-06-04 23:32:04 -0500

    Update AccountController.cs
    Debería redireccionar si un admin se logea

--- ProyectoSaberProWeb/Controllers/AccountController.cs
+++ ProyectoSaberProWeb/Controllers/AccountController.cs
@@ -96,7 +96,7 @@ namespace ProyectoSaberProWeb.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return RedirectToAction("Index", "Administrador");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
